2819f1d3cafcb55296aa3bab67b59abb36bbb385
Declan Van Welie <declanv@gmail.com>
2019-12-17 22:34:52 -0500

    Updates

---
+++ precache-manifest.9615fcc1a2e153b719c46ae50b1e3ab4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7411f6328ddcb5aa99cd92f0cc89c63a",
+    "url": "/burn-report/index.html"
+  },
+  {
+    "revision": "bd9abd6f110b5c0503de",
+    "url": "/burn-report/static/css/main.344d7d4a.chunk.css"
+  },
+  {
+    "revision": "ed5be0e5b0dfd2cac3a9",
+    "url": "/burn-report/static/js/2.830f7ef1.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/burn-report/static/js/2.830f7ef1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd9abd6f110b5c0503de",
+    "url": "/burn-report/static/js/main.f2d5edf7.chunk.js"
+  },
+  {
+    "revision": "e47c9c82874d3bd68e45",
+    "url": "/burn-report/static/js/runtime-main.da766c80.js"
+  },
+  {
+    "revision": "62f16e775587756adf5ed13031001488",
+    "url": "/burn-report/static/media/MatchbookTexture.62f16e77.otf"
+  },
+  {
+    "revision": "7e82d79b71f360f1ec68797d34e8608a",
+    "url": "/burn-report/static/media/sun.7e82d79b.svg"
+  }
+]);
